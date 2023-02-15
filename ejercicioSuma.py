f8677ca1d4523d6d1aad5d980aba08f1a4cdfd3d
JuanesZg <juanesgutierrez64@gmail.com>
2023-02-15 11:32:43 -0500

    Update multiplicacion y division

--- ejercicioSuma.py
+++ ejercicioSuma.py
@@ -10,7 +10,12 @@ Comment
 suma = num1 + num2
 print(suma)
 
-"""
-
 resta = num2 - num1 
 print(resta)
+"""
+
+multiplicacion = num1 * num2
+print(multiplicacion)
+
+division = num2/num1
+print(division)
