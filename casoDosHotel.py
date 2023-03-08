0c49808e486a6491d8064cefa0e7c302c23e2e5e
JuanesZg <juanesgutierrez64@gmail.com>
2023-03-08 07:04:53 -0500

    Correcciones Caso Dos y Ejercicio 1 de divisas

--- casoDosHotel.py
+++ casoDosHotel.py
@@ -10,19 +10,16 @@ while controlCiclo != "0":
         costoEstadia = (tIndividual * diasEstadia)
         costoEstadiaIVA = costoEstadia + (costoEstadia*0.19)
         costoEstadiaDescuento = costoEstadiaIVA - (costoEstadiaIVA*0.05)
-        print("El costo de su estadia de", diasEstadia,"dias es de",int(costoEstadia),"Sin IVA,",int(costoEstadiaIVA),"con IVA y,",int(costoEstadiaDescuento),"con el descuento sobre el IVA del 5%\n")
+        print("El costo de su estadia de", diasEstadia,"dias es de $",int(costoEstadia),"Sin IVA, $",int(costoEstadiaIVA),"con IVA y, $",int(costoEstadiaDescuento),"con el descuento sobre el IVA del 5%\n")
     elif planEstadia == 2:
         costoEstadia = (tDoble * diasEstadia)
         costoEstadiaIVA = costoEstadia + (costoEstadia*0.19)
         costoEstadiaDescuento = costoEstadiaIVA - (costoEstadiaIVA*0.09)
-        print("El costo de su estadia de", diasEstadia,"dias es de",int(costoEstadia),"Sin IVA,",int(costoEstadiaIVA),"con IVA y,",int(costoEstadiaDescuento),"con el descuento sobre el IVA del 9%\n")
+        print("El costo de su estadia de", diasEstadia,"dias es de $",int(costoEstadia),"Sin IVA, $",int(costoEstadiaIVA),"con IVA y, $",int(costoEstadiaDescuento),"con el descuento sobre el IVA del 9%\n")
     elif planEstadia == 3:
         costoEstadia = (tFamiliar * diasEstadia)
         costoEstadiaIVA = costoEstadia + (costoEstadia*0.19)
         costoEstadiaDescuento = costoEstadiaIVA - (costoEstadiaIVA*0.15)
-        print("El costo de su estadia de", diasEstadia,"dias es de",int(costoEstadia),"Sin IVA,",int(costoEstadiaIVA),"con IVA y,",int(costoEstadiaDescuento),"con el descuento sobre el IVA del 15%\n")
-    elif planEstadia == 4:
-        controlCiclo = 1
-        print("Vuelva pronto!")
+        print("El costo de su estadia de", diasEstadia,"dias es de $",int(costoEstadia),"Sin IVA, $",int(costoEstadiaIVA),"con IVA y, $",int(costoEstadiaDescuento),"con el descuento sobre el IVA del 15%\n")
     controlCiclo= input("Presione 0 para terminar el programa, o cualquier otra tecla para repetirlo\n")
         
